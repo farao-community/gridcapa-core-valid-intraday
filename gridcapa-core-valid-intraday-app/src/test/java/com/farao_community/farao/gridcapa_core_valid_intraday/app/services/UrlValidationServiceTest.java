c3330127d20197cb3a6d4df19025f05099e5baab
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-10-24 09:25:01 +0200

    add urlvalidation test

--- gridcapa-core-valid-intraday-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_intraday/app/services/UrlValidationServiceTest.java
+++ gridcapa-core-valid-intraday-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_intraday/app/services/UrlValidationServiceTest.java
@@ -1,12 +1,21 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.gridcapa_core_valid_intraday.app.services;
 
 import com.farao_community.farao.gridcapa_core_valid_intraday.api.exception.CoreValidIntradayInvalidDataException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Marc Schwitzguebel {@literal <marc.schwitzguebel_external at rte-france.com>}
+ */
 @SpringBootTest
 class UrlValidationServiceTest {
     @Autowired
@@ -14,9 +23,9 @@ class UrlValidationServiceTest {
 
     @Test
     void checkExceptionThrownWhenUrlIsNotPartOfWhitelistedUrls() {
-        Exception exception = assertThrows(CoreValidIntradayInvalidDataException.class, () -> urlValidationService.openUrlStream("url1"));
+        Exception exception = Assertions.assertThrows(CoreValidIntradayInvalidDataException.class, () -> urlValidationService.openUrlStream("url1"));
         String expectedMessage = "is not part of application's whitelisted url's";
         String actualMessage = exception.getMessage();
-        assertTrue(actualMessage.contains(expectedMessage));
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 }
