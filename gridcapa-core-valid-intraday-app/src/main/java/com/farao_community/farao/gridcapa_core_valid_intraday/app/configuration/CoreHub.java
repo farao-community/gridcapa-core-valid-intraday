460feac4e83b02020fced71d065636a6dbd03925
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-10-29 12:03:21 +0100

    init core hub configuration

---
+++ gridcapa-core-valid-intraday-app/src/main/java/com/farao_community/farao/gridcapa_core_valid_intraday/app/configuration/CoreHub.java
@@ -0,0 +1,4 @@
+package com.farao_community.farao.gridcapa_core_valid_intraday.app.configuration;
+
+public record CoreHub(String name, String ramcep2Code, String flowbasedCode, String forecastCode, String clusterVerticeCode, boolean isHvdcHub, double coefficient) {
+}
