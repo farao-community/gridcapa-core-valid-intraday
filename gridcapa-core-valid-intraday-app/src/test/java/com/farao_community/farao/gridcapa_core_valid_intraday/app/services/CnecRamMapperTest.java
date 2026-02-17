c5381b1e1089df0d93bba11ad846328ce883c190
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2026-02-17 13:53:25 +0100

    Fix checkstyle

--- gridcapa-core-valid-intraday-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_intraday/app/services/CnecRamMapperTest.java
+++ gridcapa-core-valid-intraday-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_intraday/app/services/CnecRamMapperTest.java
@@ -16,14 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.net.URL;
-import java.time.OffsetDateTime;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CnecRamMapperTest {
-
 
     @Autowired
     private FileImporter fileImporter;
